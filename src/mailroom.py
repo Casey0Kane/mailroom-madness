edb9bd653d65e9cb72bc3d58c7c452a246317e5f
Ronel Rustia <fedstats@yahoo.com>
2017-05-21 06:17:37 -0700

    implemented some functions

--- src/mailroom.py
+++ src/mailroom.py
@@ -1,37 +1,108 @@
 """Pseudo code for mailroom madness project 401 python."""
 
+import sys
+import io
+DATABASE = {}
+
 
 def initial_prompt():
-    """Prompts the user to send a Thank you or Create a report."""
+    """Prompt the user to send a Thank you or Create a report."""
+    choice = input("Send a (T)hank You Note or (C)reate a Report, or (Q)uit: ")
+    return choice
+
+
+def initialize_dictionary():
+    """Open database file call donators.txt and construct the dictionary."""
+    f = io.open("donators.txt", "r")
+    for word in list(f.readline().split(',')):
+        if not word.isnumeric():
+            key = word.lower()
+            amount_list = []
+        else:
+            amount_list.append(int(word))
+        DATABASE[key] = amount_list
+    f.close()
+    print("dictionary is : ", DATABASE)  # for debugging purpose
 
 
 def quit_prompt():
     """Prompt user to quit or restart from the beginning."""
+    choice = input("Go back to the (T)op Menu or (K)eep going")
+    return choice
 
 
 def donor_name_list():
     """Prompt user to enter a name or view the donor list."""
+    choice = input("Please enter a name to add a donor or 'list' to list existing donors.")
+    return choice
 
 
 def list_of_donors():
     """Print a list of donors."""
+    for name in DATABASE:
+        print(name)
 
 
 def create_report():
     """Print list of donor names and amount donated."""
+    print("Name            ")
 
 
 def donation():
     """Prompt the user for a valid value to donate."""
+    amount = input("Please input a dollar amount to be added to the Donator's history : ")
+    return amount
 
 
-def update_donation():
+def update_donation(name, amount):
     """Update donation value and save it to text file."""
+    DATABASE[name].append(int(amount))
 
 
 def create_text_file():
-    """Write list of donors to text file."""
+    """Write list of donors to text file named output.txt."""
+    output_file = open("output.txt", "w")
+    first_key = True
+    for key, val in DATABASE.items():
+        if first_key:
+            output_file.write(key)
+        else:
+            output_file.write("," + key)
+        for x in val:
+            output_file.write("," + str(x))
+        first_key = False
+    output_file.close()
 
 
-def create_email():
-    """Sends thank you email to donor."""
+def create_email(name):
+    """Send thank you email to donor."""
+    """This will print email to screen then user can just print-screen."""
+    last_donation = DATABASE[name][len(DATABASE[name]) - 1]
+    print("Dear {0},\n\n        Thank you so much for donating to our ch\
+arity with an amount of {1}.\n\n        Your support is very much apprec\
+iated.  We look forward to your support in 2018.\n\n\n\
+                                                              Regards,\n\
+                                                              John Doe\n\
+                                         Community Support Coordinator\
+    ".format(name, last_donation))
+
+
+def main():
+    """Main program."""
+    initialize_dictionary()
+    choice = initial_prompt().lower()
+    while choice != "q":
+        if choice == 't':
+            list_of_donors()
+            DATABASE[input("Please type a donor name: ")].append(float(input("Please enter a dollar amount: ")))
+        elif choice == 'c':
+            print('it"s a C')
+        else:
+            if choice == 'q':
+                sys.exit()
+        choice = initial_prompt().lower()
+
+
+if __name__ == "__main__":
+    """Handle console user input"""
+    main()
